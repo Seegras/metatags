0776332faf17d9d5492fe2a368c6bdf3575498eb
Peter Keel <git@discordia.ch>
2020-05-07 00:34:43 +0200

    mkvsrtattach.sh: shellcheck'ed

--- mkvsrtattach.sh
+++ mkvsrtattach.sh
@@ -12,43 +12,43 @@ if ! command -v mkvmerge >/dev/null 2>&1; then
     exit 1
 fi
 
-debug=1
-for filename in *.srt; do 
-    filename_no_ext=$(basename "$filename" .srt)
+DEBUG=1
+for FILE in *.srt; do 
+    filename_no_ext="$( basename "$FILE" .srt )"
     # easy case, just attach the subtitle
     if [ -e "$filename_no_ext.mkv" ]
     then
-	mkvmerge -o $filename_no_ext.OUT.mkv $filename_no_ext.mkv --language 0:eng $filename
-	    mv ${filename_no_ext}.OUT.mkv ${filename_no_ext}.mkv
-	    rm $filename
+        mkvmerge -o "$filename_no_ext".OUT.mkv "$filename_no_ext".mkv --language 0:eng "${FILE}"
+            mv "${filename_no_ext}".OUT.mkv "${filename_no_ext}".mkv
+            rm "${FILE}"
     fi
-    IFS='-' read -ra fields <<< "$filename_no_ext"
-    if [ $debug ]; then set | grep ^fields=\\\|^var=; fi
+    IFS='-' read -ra fields <<< "${filename_no_ext}"
+    if [ "${DEBUG}" ]; then set | grep ^fields=\\\|^var=; fi
     fnum=$((${#fields[@]}-1))
-    if [ $debug ]; then echo $fnum; fi
+    if [ ${DEBUG} ]; then echo ${fnum}; fi
     # in this case, the ending is probably a language code
-    if [ ${#fields[$fnum]} -eq 3 ]
+    if [ ${#fields[${fnum}]} -eq 3 ]
     then
-	mkvfields=$[fnum-1]
-	i=1
-	base=${fields[0]}
-	while [ $i -lt $mkvfields ]
-	do
-	    base+="-"
-	    base+=${fields[$i]}
-	    i=$[$i+1]
-	done
-	if [ $debug ]; then echo $base; fi
-	langcode=${fields[$fnum]}
-	declare -l langcode
-	if [ $debug ]; then echo $langcode; fi
-	# we now hopefully have the filename 
+        mkvfields=$(fnum-1)
+        i=1
+        base=${fields[0]}
+        while [ "${i}" -lt "${mkvfields}" ]
+        do
+            base+="-"
+            base+=${fields[$i]}
+            i=$(i+1)
+        done
+        if [ ${DEBUG} ]; then echo "${base}"; fi
+        LANGCODE=${fields[$fnum]}
+        declare -l LANGCODE
+        if [ ${DEBUG} ]; then echo "${LANGCODE}"; fi
+        # we now hopefully have the filename 
         if [ -e "$base.mkv" ]
-	then
-	    mkvmerge -o ${base}.OUT.mkv ${base}.mkv --language 0:$langcode $filename
-	    mv ${base}.OUT.mkv ${base}.mkv
-	    rm $filename
-	fi
+        then
+            mkvmerge -o "${base}".OUT.mkv "${base}".mkv --language 0:"${LANGCODE}" "${FILE}"
+            mv "${base}".OUT.mkv "${base}".mkv
+            rm "${FILE}"
+        fi
     fi
 
 done
