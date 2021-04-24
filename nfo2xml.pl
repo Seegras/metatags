35b565877650cfedee64d6d6a4bb551221920c6e
Peter Keel <git@discordia.ch>
2021-04-24 16:28:47 +0200

    nfo2xml: handle case if there's only one actor

--- nfo2xml.pl
+++ nfo2xml.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 # 
 # nfo2xml -- converts xbmc metadata .nfo-files to Matroshka metadata 
-#	     XML files.
+#             XML files.
 # 
 # Author:  Peter Keel <seegras@discordia.ch>
 # Date:    2013-09-21
@@ -52,179 +52,190 @@ closedir($in_dir);
     my $name; 
     my $suffix;
     ($name,$suffix) = $filename =~ /^(.*)(\.[^.]*)$/;
-	if ($filename ne ".." and $filename ne "." and $suffix eq ".nfo") {
-	# create object
-	my $xml = new XML::Simple;
-	# read XML file
-	my $data = $xml->XMLin("$dname/$filename", suppressempty => '');
-	open (my $out_xml, ">:encoding(UTF-8)","$dname/$name.xml");
-	print $out_xml "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-	print $out_xml "<!DOCTYPE Tags SYSTEM \"matroskatags.dtd\">\n";
-	print $out_xml "<Tags>\n";
-	# series
-	if ($data->{showtitle} ne "" || $data->{set} ne "") {
-	    print $out_xml "  <Tag>\n";
-	    print $out_xml "    <Targets>\n";
-	    print $out_xml "      <TargetTypeValue>70</TargetTypeValue>\n";
-	    print $out_xml "    </Targets>\n";
-	    if ($data->{set} ne "") {
-		print $out_xml "    <Simple>\n";
-		print $out_xml "      <Name>COLLECTION</Name>\n";
-		print $out_xml "      <String>$data->{set}</String>\n";
-		print $out_xml "    </Simple>\n";
-	    }
-	    print $out_xml "    <Simple>\n";
-	    print $out_xml "      <Name>TITLE</Name>\n";
-	    if ($data->{showtitle} ne "" && $data->{set} eq "") {
-		print $out_xml "      <String>$data->{showtitle}</String>\n";
-	    } elsif ($data->{showtitle} eq "" && $data->{set} ne "") {
-		print $out_xml "      <String>$data->{set}</String>\n";
-	    } 
-	    print $out_xml "    </Simple>\n";
-	    if(ref($data->{genre}) eq 'ARRAY') {
-	        foreach my $genre (@{$data->{genre}}) {
-		    print $out_xml "    <Simple>\n";
-	            print $out_xml "    <Name>GENRE</Name>\n";
-		    print $out_xml "	<String>$genre</String>\n";
-	            print $out_xml "    </Simple>\n";
-	        }
-	    } elsif ($data->{genre} ne "") {
-	        print $out_xml "    <Simple>\n";
-	        print $out_xml "      <Name>GENRE</Name>\n";
-	        print $out_xml "      <String>$data->{genre}</String>\n";
-	        print $out_xml "    </Simple>\n";
-	    }
-	    print $out_xml "  </Tag>\n";
-	} 
-	if ($data->{season} ne "") {
-	    print $out_xml "  <Tag>\n";
-	    print $out_xml "    <Targets>\n";
-	    print $out_xml "      <TargetTypeValue>60</TargetTypeValue>\n";
-	    print $out_xml "    </Targets>\n";
-	    print $out_xml "    <Simple>\n";
-	    print $out_xml "      <Name>PART_NUMBER</Name>\n";
-	    print $out_xml "      <String>$data->{season}</String>\n";
-	    print $out_xml "    </Simple>\n";
-	    # TOTAL_PARTS unknown
-	    # print $out_xml "    <Simple>\n";
-	    # print $out_xml "      <Name>TOTAL_PARTS</Name>\n";
-	    # print $out_xml "      <String>13</String>\n";
-	    # print $out_xml "    </Simple>\n";
-	    print $out_xml "  </Tag>\n";
-	}
-	print $out_xml "  <Tag>\n";
-	print $out_xml "    <Targets>\n";
-	print $out_xml "      <TargetTypeValue>50</TargetTypeValue>\n";
-	print $out_xml "    </Targets>\n";
-	print $out_xml "    <Simple>\n";
-	print $out_xml "      <Name>TITLE</Name>\n";
-	print $out_xml "      <String>$data->{title}</String>\n";
-	print $out_xml "    </Simple>\n";
-	if ($data->{originaltitle} ne "") {
-	    print $out_xml "    <Simple>\n";
-	    print $out_xml "      <Name>ORIGINAL</Name>\n";
-	    print $out_xml "      <String>$data->{originaltitle}</String>\n";
-	    print $out_xml "    </Simple>\n";
-	}
-	if ($data->{plot} ne "") {
-	    print $out_xml "    <Simple>\n";
-	    print $out_xml "    <Name>SUMMARY</Name>\n";
-	    print $out_xml "	<String>$data->{plot}</String>\n";
-	    print $out_xml "    </Simple>\n";
-	}
-	if ($data->{episode} ne "") {
-	    print $out_xml "    <Simple>\n";
-	    print $out_xml "      <Name>PART_NUMBER</Name>\n";
-	    print $out_xml "      <String>$data->{episode}</String>\n";
-	    print $out_xml "    </Simple>\n";
-	}
-	if ($data->{year} ne "" || $data->{aired} ne "") {
-	    print $out_xml "    <Simple>\n";
-	    print $out_xml "      <Name>DATE_RELEASED</Name>\n";
-	    if ($data->{year} ne "" && $data->{year} ne "0") {
-		print $out_xml "      <String>$data->{year}</String>\n";
-	    } elsif ($data->{aired} ne "" && $data->{aired} ne "1969-12-31") {
-		print $out_xml "      <String>$data->{aired}</String>\n";
-	    } elsif ($data->{premiered} ne "" && $data->{premiered} ne "1969-12-31") {
-		print $out_xml "      <String>$data->{premiered}</String>\n";
-	    }
-	    print $out_xml "    </Simple>\n";
-	}
-	if(ref($data->{director}) eq 'ARRAY') {
-	    foreach my $directors (@{$data->{director}}) {
-		print $out_xml "    <Simple>\n";
-		print $out_xml "      <Name>DIRECTOR</Name>\n";
-		print $out_xml "      <String>$directors</String>\n";
-		print $out_xml "    </Simple>\n";
-	    }
-	} elsif ($data->{director} ne "") {
-	    print $out_xml "    <Simple>\n";
-	    print $out_xml "      <Name>DIRECTOR</Name>\n";
-	    print $out_xml "      <String>$data->{director}</String>\n";
-	    print $out_xml "    </Simple>\n";
-	}
-	foreach my $actors (keys %{$data->{actor}}) {
-	    print $out_xml "    <Simple>\n";
-	    print $out_xml "      <Name>ACTOR</Name>\n";
-	    print $out_xml "      <String>$actors</String>\n";
-	    print $out_xml "      <Simple>\n";
-	    print $out_xml "        <Name>CHARACTER</Name>\n";
-	    print $out_xml "        <String>$data->{actor}->{$actors}->{role}</String>\n";
-	    print $out_xml "      </Simple>\n";
-	    print $out_xml "    </Simple>\n";
-	}
-	if(ref($data->{credits}) eq 'ARRAY') {
-	    foreach my $credits (@{$data->{credits}}) {
-		print $out_xml "    <Simple>\n";
-		print $out_xml "      <Name>THANKS_TO</Name>\n";
-		print $out_xml "      <String>$credits</String>\n";
-		print $out_xml "    </Simple>\n";
-	    }
-	} elsif ($data->{credits} ne "") {
-	    print $out_xml "    <Simple>\n";
-	    print $out_xml "      <Name>THANKS_TO</Name>\n";
-	    print $out_xml "      <String>$data->{credits}</String>\n";
-	    print $out_xml "    </Simple>\n";
-	}
-	# only print genre here if no series
-	if ($data->{showtitle} eq "") {
-	    if(ref($data->{genre}) eq 'ARRAY') {
-	        foreach my $genre (@{$data->{genre}}) {
-		    print $out_xml "    <Simple>\n";
-	            print $out_xml "    <Name>GENRE</Name>\n";
-		    print $out_xml "	<String>$genre</String>\n";
-	            print $out_xml "    </Simple>\n";
-	        }
-	    } elsif ($data->{genre} ne "") {
-	        print $out_xml "    <Simple>\n";
-	        print $out_xml "      <Name>GENRE</Name>\n";
-	        print $out_xml "      <String>$data->{genre}</String>\n";
-	        print $out_xml "    </Simple>\n";
-	    }
-	}
-	
-	if ($data->{tmdbid} ne "") {
-	    print $out_xml "    <Simple>\n";
-	    print $out_xml "      <Name>CATALOG_NUMBER</Name>\n";
-	    print $out_xml "      <String>$data->{tmdbid}</String>\n";
-	    print $out_xml "    </Simple>\n";
-	}
-	if ($data->{id} ne "") {
-	    print $out_xml "    <Simple>\n";
-	    print $out_xml "      <Name>CATALOG_NUMBER</Name>\n";
-	    print $out_xml "      <String>$data->{id}</String>\n";
-	    print $out_xml "    </Simple>\n";
-	}
-	if ($data->{uniqueid} ne "") {
-	    print $out_xml "    <Simple>\n";
-	    print $out_xml "      <Name>CATALOG_NUMBER</Name>\n";
-	    print $out_xml "      <String>$data->{uniqueid}</String>\n";
-	    print $out_xml "    </Simple>\n";
-	}
-	print $out_xml "  </Tag>\n";
-	print $out_xml "</Tags>\n";
-	close ($out_xml); 
-	}
+        if ($filename ne ".." and $filename ne "." and $suffix eq ".nfo") {
+        # create object
+        my $xml = new XML::Simple;
+        # read XML file
+        my $data = $xml->XMLin("$dname/$filename", suppressempty => '');
+        open (my $out_xml, ">:encoding(UTF-8)","$dname/$name.xml");
+        print $out_xml "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+        print $out_xml "<!DOCTYPE Tags SYSTEM \"matroskatags.dtd\">\n";
+        print $out_xml "<Tags>\n";
+        # series
+        if ($data->{showtitle} ne "" || $data->{set} ne "") {
+            print $out_xml "  <Tag>\n";
+            print $out_xml "    <Targets>\n";
+            print $out_xml "      <TargetTypeValue>70</TargetTypeValue>\n";
+            print $out_xml "    </Targets>\n";
+            if ($data->{set} ne "") {
+                print $out_xml "    <Simple>\n";
+                print $out_xml "      <Name>COLLECTION</Name>\n";
+                print $out_xml "      <String>$data->{set}</String>\n";
+                print $out_xml "    </Simple>\n";
+            }
+            print $out_xml "    <Simple>\n";
+            print $out_xml "      <Name>TITLE</Name>\n";
+            if ($data->{showtitle} ne "" && $data->{set} eq "") {
+                print $out_xml "      <String>$data->{showtitle}</String>\n";
+            } elsif ($data->{showtitle} eq "" && $data->{set} ne "") {
+                print $out_xml "      <String>$data->{set}</String>\n";
+            } 
+            print $out_xml "    </Simple>\n";
+            if(ref($data->{genre}) eq 'ARRAY') {
+                foreach my $genre (@{$data->{genre}}) {
+                    print $out_xml "    <Simple>\n";
+                    print $out_xml "    <Name>GENRE</Name>\n";
+                    print $out_xml "        <String>$genre</String>\n";
+                    print $out_xml "    </Simple>\n";
+                }
+            } elsif ($data->{genre} ne "") {
+                print $out_xml "    <Simple>\n";
+                print $out_xml "      <Name>GENRE</Name>\n";
+                print $out_xml "      <String>$data->{genre}</String>\n";
+                print $out_xml "    </Simple>\n";
+            }
+            print $out_xml "  </Tag>\n";
+        } 
+        if ($data->{season} ne "") {
+            print $out_xml "  <Tag>\n";
+            print $out_xml "    <Targets>\n";
+            print $out_xml "      <TargetTypeValue>60</TargetTypeValue>\n";
+            print $out_xml "    </Targets>\n";
+            print $out_xml "    <Simple>\n";
+            print $out_xml "      <Name>PART_NUMBER</Name>\n";
+            print $out_xml "      <String>$data->{season}</String>\n";
+            print $out_xml "    </Simple>\n";
+            # TOTAL_PARTS unknown
+            # print $out_xml "    <Simple>\n";
+            # print $out_xml "      <Name>TOTAL_PARTS</Name>\n";
+            # print $out_xml "      <String>13</String>\n";
+            # print $out_xml "    </Simple>\n";
+            print $out_xml "  </Tag>\n";
+        }
+        print $out_xml "  <Tag>\n";
+        print $out_xml "    <Targets>\n";
+        print $out_xml "      <TargetTypeValue>50</TargetTypeValue>\n";
+        print $out_xml "    </Targets>\n";
+        print $out_xml "    <Simple>\n";
+        print $out_xml "      <Name>TITLE</Name>\n";
+        print $out_xml "      <String>$data->{title}</String>\n";
+        print $out_xml "    </Simple>\n";
+        if ($data->{originaltitle} ne "") {
+            print $out_xml "    <Simple>\n";
+            print $out_xml "      <Name>ORIGINAL</Name>\n";
+            print $out_xml "      <String>$data->{originaltitle}</String>\n";
+            print $out_xml "    </Simple>\n";
+        }
+        if ($data->{plot} ne "") {
+            print $out_xml "    <Simple>\n";
+            print $out_xml "    <Name>SUMMARY</Name>\n";
+            print $out_xml "        <String>$data->{plot}</String>\n";
+            print $out_xml "    </Simple>\n";
+        }
+        if ($data->{episode} ne "") {
+            print $out_xml "    <Simple>\n";
+            print $out_xml "      <Name>PART_NUMBER</Name>\n";
+            print $out_xml "      <String>$data->{episode}</String>\n";
+            print $out_xml "    </Simple>\n";
+        }
+        if ($data->{year} ne "" || $data->{aired} ne "") {
+            print $out_xml "    <Simple>\n";
+            print $out_xml "      <Name>DATE_RELEASED</Name>\n";
+            if ($data->{year} ne "" && $data->{year} ne "0") {
+                print $out_xml "      <String>$data->{year}</String>\n";
+            } elsif ($data->{aired} ne "" && $data->{aired} ne "1969-12-31") {
+                print $out_xml "      <String>$data->{aired}</String>\n";
+            } elsif ($data->{premiered} ne "" && $data->{premiered} ne "1969-12-31") {
+                print $out_xml "      <String>$data->{premiered}</String>\n";
+            }
+            print $out_xml "    </Simple>\n";
+        }
+        if(ref($data->{director}) eq 'ARRAY') {
+            foreach my $directors (@{$data->{director}}) {
+                print $out_xml "    <Simple>\n";
+                print $out_xml "      <Name>DIRECTOR</Name>\n";
+                print $out_xml "      <String>$directors</String>\n";
+                print $out_xml "    </Simple>\n";
+            }
+        } elsif ($data->{director} ne "") {
+            print $out_xml "    <Simple>\n";
+            print $out_xml "      <Name>DIRECTOR</Name>\n";
+            print $out_xml "      <String>$data->{director}</String>\n";
+            print $out_xml "    </Simple>\n";
+        }
+        if(ref($data->{actor}) eq 'ARRAY') {
+            foreach my $actors (keys %{$data->{actor}}) {
+                print $out_xml "    <Simple>\n";
+                print $out_xml "      <Name>ACTOR</Name>\n";
+                print $out_xml "      <String>$actors</String>\n";
+                print $out_xml "      <Simple>\n";
+                print $out_xml "        <Name>CHARACTER</Name>\n";
+                print $out_xml "        <String>$data->{actor}->{$actors}->{role}</String>\n";
+                print $out_xml "      </Simple>\n";
+                print $out_xml "    </Simple>\n";
+            }
+        } elsif ($data->{actor} ne "") {
+            print $out_xml "    <Simple>\n";
+            print $out_xml "      <Name>ACTOR</Name>\n";
+            print $out_xml "      <String>$data->{actor}->{name}</String>\n";
+            print $out_xml "      <Simple>\n";
+            print $out_xml "        <Name>CHARACTER</Name>\n";
+            print $out_xml "        <String>$data->{actor}->{role}</String>\n";
+            print $out_xml "      </Simple>\n";
+            print $out_xml "    </Simple>\n";
+        }
+        if(ref($data->{credits}) eq 'ARRAY') {
+            foreach my $credits (@{$data->{credits}}) {
+                print $out_xml "    <Simple>\n";
+                print $out_xml "      <Name>THANKS_TO</Name>\n";
+                print $out_xml "      <String>$credits</String>\n";
+                print $out_xml "    </Simple>\n";
+            }
+        } elsif ($data->{credits} ne "") {
+            print $out_xml "    <Simple>\n";
+            print $out_xml "      <Name>THANKS_TO</Name>\n";
+            print $out_xml "      <String>$data->{credits}</String>\n";
+            print $out_xml "    </Simple>\n";
+        }
+        # only print genre here if no series
+        if ($data->{showtitle} eq "") {
+            if(ref($data->{genre}) eq 'ARRAY') {
+                foreach my $genre (@{$data->{genre}}) {
+                    print $out_xml "    <Simple>\n";
+                    print $out_xml "    <Name>GENRE</Name>\n";
+                    print $out_xml "        <String>$genre</String>\n";
+                    print $out_xml "    </Simple>\n";
+                }
+            } elsif ($data->{genre} ne "") {
+                print $out_xml "    <Simple>\n";
+                print $out_xml "      <Name>GENRE</Name>\n";
+                print $out_xml "      <String>$data->{genre}</String>\n";
+                print $out_xml "    </Simple>\n";
+            }
+        }
+        
+        if ($data->{tmdbid} ne "") {
+            print $out_xml "    <Simple>\n";
+            print $out_xml "      <Name>CATALOG_NUMBER</Name>\n";
+            print $out_xml "      <String>$data->{tmdbid}</String>\n";
+            print $out_xml "    </Simple>\n";
+        }
+        if ($data->{id} ne "") {
+            print $out_xml "    <Simple>\n";
+            print $out_xml "      <Name>CATALOG_NUMBER</Name>\n";
+            print $out_xml "      <String>$data->{id}</String>\n";
+            print $out_xml "    </Simple>\n";
+        }
+        if ($data->{uniqueid} ne "") {
+            print $out_xml "    <Simple>\n";
+            print $out_xml "      <Name>CATALOG_NUMBER</Name>\n";
+            print $out_xml "      <String>$data->{uniqueid}</String>\n";
+            print $out_xml "    </Simple>\n";
+        }
+        print $out_xml "  </Tag>\n";
+        print $out_xml "</Tags>\n";
+        close ($out_xml); 
+        }
     }
 
 __END__
