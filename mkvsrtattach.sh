21835ec9393e226346953173d0aee4f76ccf6075
Peter Keel <git@discordia.ch>
2022-09-14 09:24:20 +0200

    I think I have fixed mkvsrtattach ;)

--- mkvsrtattach.sh
+++ mkvsrtattach.sh
@@ -12,7 +12,7 @@ if ! command -v mkvmerge >/dev/null 2>&1; then
     exit 1
 fi
 
-DEBUG=0
+DEBUG=1
 for FILE in ./*.srt; do 
     filename_no_ext="$( basename "$FILE" .srt )"
     # easy case, just attach the subtitle
@@ -26,28 +26,34 @@ for FILE in ./*.srt; do
     if [[ "${DEBUG}" ]]; then set | grep ^fields=\\\|^var=; fi
     fnum=$((${#fields[@]}-1))
     if [[ ${DEBUG} ]]; then echo ${fnum}; fi
-    # in this case, the ending is probably a language code
-    if [[ ${#fields[${fnum}]} -eq 3 ]]
+    # in this case the ending is probably a language code
+    if [[ ${fnum} -eq 2 ]] 
     then
-        mkvfields=$(fnum-1)
-        i=1
-        base=${fields[0]}
-        while [ "${i}" -lt "${mkvfields}" ]
-        do
-            base+="-"
-            base+=${fields[$i]}
-            i=$(i+1)
-        done
-        if [[ ${DEBUG} ]]; then echo "${base}"; fi
-        LANGCODE=${fields[$fnum]}
-        declare -l LANGCODE
-        if [ ${DEBUG} ]; then echo "${LANGCODE}"; fi
-        # we now hopefully have the filename 
-        if [ -e "$base.mkv" ]
-        then
-            mkvmerge -o "${base}".OUT.mkv "${base}".mkv --language 0:"${LANGCODE}" "${FILE}"
-            mv "${base}".OUT.mkv "${base}".mkv
-            rm "${FILE}"
-        fi
+        base="${fields[0]}"
+    fi
+    if [[ ${fnum} -eq 3 ]] 
+    then
+        base="${fields[0]}-${fields[1]}"
+    fi
+    if [[ ${fnum} -eq 4 ]]
+    then
+        base="${fields[0]}-${fields[1]}-${fields[2]}"
+    fi
+    #mkvfields=${fnum}
+    #cnt=1
+    #for ((cnt=1;cnt<$fnum;++cnt)) do
+    #    base+="-"
+    #    base+=${fields[$cnt]}
+    #done
+    if [[ ${DEBUG} ]]; then echo "${base}"; fi
+    LANGCODE=${fields[$fnum]}
+    declare -l LANGCODE
+    if [ ${DEBUG} ]; then echo "${LANGCODE}"; fi
+    # we now hopefully have the filename 
+    if [ -e "$base.mkv" ]
+    then
+        mkvmerge -o "${base}".OUT.mkv "${base}".mkv --language 0:"${LANGCODE}" "${FILE}"
+        mv "${base}".OUT.mkv "${base}".mkv
+        rm "${FILE}"
     fi
 done
