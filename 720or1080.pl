b9a7db5688b8c339a71444ebdeb59fd250e0cf1b
Peter Keel <git@discordia.ch>
2022-09-20 10:15:43 +0200

    720or1080: Make it more reliable using mediainfo

--- 720or1080.pl
+++ 720or1080.pl
@@ -5,7 +5,8 @@
 # Author:  Peter Keel <seegras@discordia.ch>
 # Date:    
 # Revised: 2019-10-16
-# Version: 0.3
+# Revised: 2022-09-20
+# Version: 0.4
 # License: Artistic License 2.0 or MIT License
 # URL:     http://seegras.discordia.ch/Programs/
 # 
@@ -14,15 +15,17 @@ use strict;
 use Getopt::Long;
 use Pod::Usage;
 
-my $debug = 1;
 my $needshelp = 0;
 my $file_name;
+my $in_file;
+my $only4k = 0;
 my @files = ();
 
 
 &Getopt::Long::Configure( 'pass_through', 'no_autoabbrev', 'bundling');
 &Getopt::Long::GetOptions(
 		'help|h'		=> \$needshelp,
+		'4K|4'		=> \$only4k,
 );
 
 die "Usage: 720or1080 filename\n"       unless($ARGV[0]);
@@ -33,27 +36,28 @@ pod2usage(1);
 }
 
 foreach my $file_name (@ARGV) {
-    open(my $in_file,"<:encoding(UTF-8)","$file_name") || die "Cannot open $file_name for input\n";
-    while(<$in_file>){
+    open(in_file, "mediainfo --inform=file://720or1080.csv $file_name|") || die "Cannot open $file_name for input\n";
+    while(<in_file>){
 	$_ =~ /(\S+) (\d+)x(\d+)/;
 	$file=$1;
 	my $width=$2;
 	my $height=$3;
+        #print "$width $height";
 	if (($width > 1920) && ($height <= 2160)) {
 	print ("2160p: ", $file, " ", $width, " ", $height, "\n");
 	}
-	if (($width > 1280) && ($width <= 1920)) {
+	if (($width > 1280) && ($width <= 1920) && ! $only4k) {
 	print ("1080p: ", $file, " ", $width, " ", $height, "\n");
 	}
-	if (($width > 720) && ($width <= 1280) && ($height <= 720)) {
+	if (($width > 720) && ($width <= 1280) && ($height <= 720) && ! $only4k) {
 	print (" 720p: ", $file, " ", $width, " ", $height, "\n");
 	}
-	if (($width <= 720) && ($height <= 576) && ($height >= 480)) {
+	if (($width <= 720) && ($height <= 576) && ($height >= 480) && ! $only4k) {
 	print ("  DVD: ", $file, " ", $width, " ", $height, "\n");
 	}
-	if (($width < 720) && ($height < 480)) {
+	if (($width < 720) && ($height < 480) && ! $only4k) {
 	print ("  LOW: ", $file, " ", $width, " ", $height, "\n");
 	}
     }
-    close $in_file;
+    close in_file;
 }
